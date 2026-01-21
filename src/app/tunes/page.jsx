e5e2d2849223767186829350b8fc5073ac77256f
Refik Jaija <refikjaija3@gmail.com>
2026-01-21 03:09:02 +0100

    Tunes update

--- src/app/tunes/page.jsx
+++ src/app/tunes/page.jsx
@@ -24,21 +24,21 @@ const songBook = [
   },
   {
     title: 'FUN-K',
-    description: 'By 2021, a distinct \'Juke Blue\' sound began to emerge. He tagged as #retrophonica on YouTube. An elderly Brazilian man commented he liked it. Two strangers sent him fan mail. That was all. Puzzled and broke, Juke started looking for a job.',
+    description: 'By 2021, a distinct \'Juke Blue\' sound had begun to emerge. He tagged the sound as #retrophonica on YouTube. An elderly Brazilian man commented he liked it. Two strangers sent him fan mail. That was all. Puzzled and broke, Juke started looking for a job.',
     iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=1701386430/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
     bandcampLink: 'https://jukeblue.bandcamp.com/track/fun-k',
   },
-  {
-    title: 'Atlantic City',
-    description: '“More money invested from a dwindling pile. This is terrible folly,” Juke warned. “Can’t you find someone else for this?” The answer was no. And so the song was made, and its crafting filmed as a pilot. The cost was considerable, leaving Juke broke. Again. ',
-    iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=1250004783/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
-    bandcampLink: 'https://jukeblue.bandcamp.com/track/atlantic-city',
-  },
-  {
+    {
     title: 'Bellissima',
-    description: 'His holy work, however, was not yet finished. A new heavenly instruction arrived late one night: go to Greece and create one final song. It would be the theme song for a TV show celebrating nature and life. It took some convincing, but Juke reluctantly agreed.',
+    description: 'His holy work, however, was not yet finished. A new heavenly instruction arrived late one night: Go to Greece and create one final song. It would be the theme tune for a TV show celebrating Nature and life. It took some convincing, but Juke reluctantly agreed.',
     iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=334099159/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
     bandcampLink: 'https://jukeblue.bandcamp.com/track/bellissima',
+  },
+  {
+    title: 'Atlantic City',
+    description: '“More money invested from a dwindling pile. This is terrible folly,” Juke warned. “Can’t you find someone else for this?” The answer was no. And so the tune was recorded, and its singing filmed as a pilot. The cost was considerable, leaving Juke broke. Again.',
+    iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=1250004783/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
+    bandcampLink: 'https://jukeblue.bandcamp.com/track/atlantic-city',
   },
   {
   title: 'Dreams and Memories',
@@ -48,31 +48,31 @@ const songBook = [
 },
 {
   title: 'Silent Freeways',
-  description: 'It came to him in the gentle lapping of water on the shoreline. He realized there was a magic in the sea, and the land, and in people everywhere that would guide the next chapter of his life. He knew what he had to do: he would bottle the essence of nature.',
+  description: 'It came to him in the gentle lapping of water on the shoreline. He realized there was a magic in the sea, the land, and in people everywhere. Magic that would open the next chapter of his life. He knew what he had to do. He would bottle the essence of Nature.',
   iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=2581956710/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
   bandcampLink: 'https://jukeblue.bandcamp.com/track/silent-freeways',
 },
 {
   title: 'Freedom',
-  description: 'But how could one capture nature when, by definition, it is free? The answer lay in alchemy: to take something from the land, the sea, and humanity, and distill it into a pure spirit—a creation that would galvanize and unite people across the hemispheres.',
+  description: 'But how could one capture Nature when it was by nature free? The answer lay in alchemy. To take something from the land, the waters, and humanity, and distill it into a pure spirit. A creation that would galvanize and unite people across the globe.',
   iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=2337143692/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
   bandcampLink: 'https://jukeblue.bandcamp.com/track/freedom',
 },
 {
   title: 'Too Young',
-  description: 'The foundation of Juke Blue’s \'Double Blue\' was forming: a high-quality natural elixir crafted from the essence of nature. Like a fine spirit aging in oak, the idea matured slowly, waiting for the right secret ingredient to temper the potion’s power.',
+  description: 'The foundation of Juke Blue’s \'Double Blue\' was forming. A high-quality natural elixir made from the essence of Nature. Like a fine spirit aging in oak, the idea matured slowly, waiting for the right secret ingredient that would ultimately temper the potion’s power.',
   iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=3205049219/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
   bandcampLink: 'https://jukeblue.bandcamp.com/track/too-young',
 },
 {
   title: 'Do the Pelican',
-  description: 'By 2025 it was ready. A holy tincture be enjoyed pure or as a cocktail, with friends in high places or companions in low ones. A finely crafted German Doppelkorn—smooth and elemental, produced organically by a family distillery with a tradition stretching deep into the past.',
+  description: 'By 2025 it was found. The result? A holy tincture that can now be enjoyed pure or as a cocktail, with friends in high places or companions in low ones. A finely crafted German Doppelkorn–smooth, elemental, and imbued with a blessing known only to its makers.',
   iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=2984575013/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
   bandcampLink: 'https://jukeblue.bandcamp.com/track/do-the-pelican',
 },
 {
   title: 'Shadowdance',
-  description: 'This commitment to nature is captured in every bottle, a spirit crafted in harmony with the land. So the next time you share a drink of Double Blue, raise a glass to nature and the unseen forces that guide you. Then listen. For therein does the adventure begin.',
+  description: 'This commitment to nature is captured in every bottle, a spirit crafted in harmony with the land. So the next time you share a drink of Double Blue, raise a glass to the unseen forces that guide you. Say “Zum Wohl”. And listen. For therein does adventure begin',
   iframeSrc: 'https://bandcamp.com/EmbeddedPlayer/track=1316116703/size=large/bgcol=333333/linkcol=fe7eaf/tracklist=false/transparent=true/',
   bandcampLink: 'https://jukeblue.bandcamp.com/track/shadowdance',
 },
